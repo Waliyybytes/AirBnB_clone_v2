bdb240dc8ff29fd243117ec0da0a9a06a63a1b22
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-31 14:09:50 +0100

    more tasks

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import environ as env
+import models
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """ State class 
+    Attributes:
+        __tablename__: table name
+        name
+        cities: relation to cities table
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    if env.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """get all cities with the current state id
+            from file storage
+            """
+            l = [
+                v for k, v in models.storage.all(models.City).items()
+                if v.state_id == self.id
+            ]
+            return (l)
